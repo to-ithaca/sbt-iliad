8e90388390e591027609434dd153930a2a196ef7
yilin <yilin.wei10@gmail.com>
2016-06-23 23:56:53 +0100

    Scoping library dependencies for desktop

--- src/main/scala/iliad/win32/settings.scala
+++ src/main/scala/iliad/win32/settings.scala
@@ -8,7 +8,7 @@ trait AllSettings {
   import iliad.common.commonKeys._
 
   /** configuration for all win32 tasks */
-  val Win32 = config("win32")
+  val Win32 = config("win32") extend Compile
 
   val settings = inConfig(Win32)(Defaults.compileSettings ++ Seq(
     sourceDirectory := (sourceDirectory in Compile).value,
@@ -16,11 +16,12 @@ trait AllSettings {
     sourceGenerators <+= generateApp,
     run <<= Defaults.runTask(fullClasspath, mainClass in run, runner in run),
     runMain <<= Defaults.runMainTask(fullClasspath, runner in run),
-    mainClass := Some(s"${targetPackage.value}.${generatedAppName.value}")
-  )) ++ Seq(
-    libraryDependencies += "com.ithaca" %% "iliad-kernel-win32" % "0.0.1-SNAPSHOT",
+    mainClass := Some(s"${targetPackage.value}.${generatedAppName.value}"),
     targetOut := target.value / "win32",
     generatedAppOut := targetOut.value / "generated"
+  )) ++ Seq(
+    ivyConfigurations += Win32,
+    libraryDependencies += "com.ithaca" %% "iliad-kernel-win32" % "0.0.1-SNAPSHOT" % Win32
   )
 }
 
